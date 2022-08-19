e1ea08fc486f833e594aa609fb4ef91fe32e2401
Luis Rodriguez <luchoweb.dev@gmail.com>
2022-08-18 19:38:17 -0500

    Alert component imprivement

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,14 +1,14 @@
 type Props = {
-  color: string,
-  icon: string,
+  color?: string,
+  icon?: string,
   message: string
 }
 
-const Alert = ( { color, icon, message }: Props ) => {
+const Alert = ( { color = 'primary', icon, message }: Props ) => {
   return (
     <div className={`alert alert-${color} text-center`}>
       <p className="m-0">
-        <i className={`fa fa-${icon}`}></i>
+        { icon && <i className={`fa fa-${icon}`}></i> }
         <span className="ms-2">{ message }</span>
       </p>
     </div>
